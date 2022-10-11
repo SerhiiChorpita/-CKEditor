34698e5e25dfac74e7017a869bc4cb674b73e648
SerhiiChorpita <sergiochorpita@gmail.com>
2022-10-11 19:23:09 +0200

    working on adding table and list. Finish

---
+++ src/app/shared/models/style.inteface.ts
@@ -0,0 +1,28 @@
+export interface IStyle {
+    textSize: string,
+    fontFamily: string,
+    color: string,
+    bgColor: string,
+    bold: boolean,
+    italic: boolean
+}
+
+export interface ITL {
+    table: boolean,
+    list: boolean
+}
+
+export interface ITable {
+    row: number,
+    cell: number,
+    width: number,
+    height: number,
+    borderWidth: number,
+    borderType: string,
+    borderColor: string
+}
+
+export interface IList {
+    lisItems: number,
+    typeMark: string
+}
